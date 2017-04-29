6be552bb48c4981735d17ea3bdd981b2f101e4f1
Mitch Ball <mitchb145@gmail.com>
2017-04-30 01:30:41 +1000

    Further linting fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 
 import './App.css';
@@ -7,19 +7,17 @@ import './App.css';
 import Home from './routes/home';
 import Test from './routes/test';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="pages">
-          <Router history={hashHistory}>
-            <Route path="/" component={Home} />
-            <Route path="/test" component={Test} />
-          </Router>
-        </div>
+function App() {
+  return (
+    <div>
+      <div className="pages">
+        <Router history={hashHistory}>
+          <Route path="/" component={Home} />
+          <Route path="/test" component={Test} />
+        </Router>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page">
+      <h1>Home</h1>
+    </div>
+  );
+}
+Home();
